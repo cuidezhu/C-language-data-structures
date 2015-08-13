7d96ccf074b6e48c0e242232edc37162aba5c165
Dezhu Cui <chinacuidezhu@gmail.com>
2015-08-13 10:30:24 +0800

    3.2 binary and storage structure

---
+++ 3-tree/3.2.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct TreeNode *BinTree;
+typedef BinTree Position;
+struct TreeNode {
+	int Data;
+	BinTree Left;
+	BinTree Right;
+};
+
+int main() {
+	
+	
+	return 0;
+}
